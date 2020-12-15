02e17319c0424b33b19ccd2ae97f335a4c8564a5
unknown <nituldeori63@gmail.com>
2020-12-16 00:52:54 +0530

    Added: Ability to add special rates for reward customers

--- Hotel.java
+++ Hotel.java
@@ -4,12 +4,16 @@ public class Hotel {
     public String hotelName;
     public int rateRegular;
     public int rateWeekend;
+    public int rateRegularReward;
+    public int rateWeekendReward;
     public int rating;
 
-    public Hotel(String hotelName,int rateRegular,int rateWeekend, int rating){
+    public Hotel(String hotelName,int rateRegular,int rateWeekend, int rateRegularReward,int rateWeekendReward, int rating){
         this.hotelName=hotelName;
         this.rateRegular=rateRegular;
         this.rateWeekend=rateWeekend;
+        this.rateRegularReward=rateRegularReward;
+        this.rateWeekendReward=rateWeekendReward;
         this.rating=rating;
     }
 
--- HotelReservationTest.java
+++ HotelReservationTest.java
@@ -40,9 +40,9 @@ public class HotelReservationTest {
     @Test
     public void testAddMethodOfHotelReservationWithWeekendRate(){
         HotelReservation HR=new HotelReservation();
-        Hotel h1=new Hotel("Lakewood",110,90,3);
-        Hotel h2=new Hotel("Bridgewood",160,50,4);
-        Hotel h3=new Hotel("Ridgewood",220,150,5);
+        Hotel h1=new Hotel("Lakewood",110,90,80,80,3);
+        Hotel h2=new Hotel("Bridgewood",160,50,110,50,4);
+        Hotel h3=new Hotel("Ridgewood",220,150,100,40,5);
         HR.addHotel(h1);
         HR.addHotel(h2);
         HR.addHotel(h3);
@@ -50,13 +50,19 @@ public class HotelReservationTest {
         Assert.assertEquals(110,HR.hotelList.get(0).rateRegular);
         Assert.assertEquals(90,HR.hotelList.get(0).rateWeekend);
         Assert.assertEquals(3,HR.hotelList.get(0).rating);
+        Assert.assertEquals(80,HR.hotelList.get(0).rateRegularReward);
+        Assert.assertEquals(80,HR.hotelList.get(0).rateWeekendReward);
         Assert.assertEquals("Bridgewood",HR.hotelList.get(1).hotelName);
         Assert.assertEquals(160,HR.hotelList.get(1).rateRegular);
         Assert.assertEquals(50,HR.hotelList.get(1).rateWeekend);
+        Assert.assertEquals(110,HR.hotelList.get(1).rateRegularReward);
+        Assert.assertEquals(50,HR.hotelList.get(1).rateWeekendReward);
         Assert.assertEquals(4,HR.hotelList.get(1).rating);
         Assert.assertEquals("Ridgewood",HR.hotelList.get(2).hotelName);
         Assert.assertEquals(220,HR.hotelList.get(2).rateRegular);
         Assert.assertEquals(150,HR.hotelList.get(2).rateWeekend);
+        Assert.assertEquals(100,HR.hotelList.get(2).rateRegularReward);
+        Assert.assertEquals(40,HR.hotelList.get(2).rateWeekendReward);
         Assert.assertEquals(5,HR.hotelList.get(2).rating);
     }
 
